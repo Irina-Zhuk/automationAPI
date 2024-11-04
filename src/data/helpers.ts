f10e26fda7d69ba7b41a93b6894dc855e9861500
Irina-Zhuk <irinamzh@gmail.com>
2024-11-04 12:28:58 -0500

    Hooks, try and catch, then, .end without Promise

--- src/data/helpers.ts
+++ src/data/helpers.ts
@@ -1,8 +1,9 @@
 import * as supertest from 'supertest'
 const request = supertest('http://localhost:8001/api/v1')
-// export async function signUp(user:string | object | undefined) {
-//     await request.post('/users/signup').send(user)
-// }
+
+export function signUp2(user:string | object | undefined) {
+    return  request.post('/users/signup').send(user)
+}
 // beginner level
 export function signUp(user:object): Promise<any> {
         return new Promise ((resolve, reject) => {
--- src/specs/user/login.spec.ts
+++ src/specs/user/login.spec.ts
@@ -1,5 +1,5 @@
 import * as supertest from 'supertest'
-import {signUp, logIn} from "../../data/helpers";
+import {signUp, logIn, signUp2} from "../../data/helpers";
 import {user, getUser} from '../../data/user';
 const request = supertest('http://localhost:8001/api/v1')
 describe('USER LOGIN', () => {
@@ -30,18 +30,74 @@ describe('USER LOGIN', () => {
             //     })
             // .expect (200)
         })
+        it('login user option 2', async () => {
+            await signUp(userImport).then (el => {
+                expect (el.body.status).toBe('success')
+                console.log(el.body, 'res')
+            })
+            await logIn({
+                email: userImport.email,
+                password : userImport.password,
+            }).then(el2 =>  {
+                expect (el2.body.status).toBe('success')
+            })
         })
+        it('login user option 3 using try and catch', async () => {
+            try {
+                    await signUp(userImport).then (el => {
+                        expect (el.body.status).toBe('success')
+                        console.log(el.body, 'res')
+                    })
+                    await logIn({
+                        email: userImport.email,
+                        password : userImport.password,
+                    }).then(el2 =>  {
+                        expect (el2.body.status).toBe('success')
+                    })
+
+            } catch (error) {
+                console.log ('Error during login process', error);
+                // throw Error (error)
+            }
+        })
+        it('login user option 4 using then', async () => {
+            signUp(userImport)
+                .then ((res) =>{
+                   expect (res.body.status).toBe('success');
+                return logIn ({
+                    email: userImport.email,
+                    password : userImport.password,
+                })
+        })
+                .then ((res2) =>{
+                    expect (res2.statusCode).toBe(201);
+        })
+                .catch ((err) => {
+                    console.log (err)
+                    })
+                })
+                })
+
+        it('login user option 5 using .end without Promise', (done) => {
+            signUp2(userImport).end ((err,res) => {
+            if(err) return done (err)
+            expect (res.body.status).toBe('success');
+            done()
+            })
+        })
+})
+
     describe('NEGATIVE TESTING', () => {
         let userImport = getUser();
-        signUp(userImport).then (el => {
-            expect (el.body.status).toBe('success')
-            console.log(el.body, 'res')
-        })
-        it('get error when trying login without email', async () => {
-            // await signUp(userImport).then (el => {
-            //     expect (el.body.status).toBe('success')
-            //     console.log(el.body, 'res')
-            // })
+        // signUp(userImport).then (el => {
+        //     expect (el.body.status).toBe('success')
+        //     console.log(el.body, 'res')
+        // })
+        it('get error when trying login without email - option 1', async () => {
+            await signUp(userImport).then (el => {     //вынесено вверх до it
+                expect (el.body.status).toBe('success')
+                console.log(el.body, 'res')
+            })
             await logIn({
                 email: '',
                 password : userImport.password,
@@ -50,25 +106,75 @@ describe('USER LOGIN', () => {
                 expect(el2.body.message).toBe('Please provide email and password!')
             })
         })
-        it('get error when trying login without password', async () => {
-            // await signUp(userImport).then (el => {
-            //     expect (el.body.status).toBe('success')
-            //     console.log(el.body, 'res')
-            // })
+        it.only('get error when trying login without email using try and catch - option 2', async () => {
+
+        try {
+            await signUp(userImport).then (el => {
+                expect (el.body.status).toBe('success')
+                console.log(el.body, 'res')
+            })
             await logIn({
-                email: userImport.email,
-                password : '',
+                email: '',
+                password : userImport.password,
             }).then(el2 =>  {
                 expect (el2.body.status).toBe('fail')
                 expect(el2.body.message).toBe('Please provide email and password!')
+                console.log(el2.body, 'res')
+            })
+
+        } catch (error) {
+            console.log ('Error during login process', error);
+            // throw Error (error)
+        }
+    })
+        it('get error when trying login without email using try and catch - option 3', async () => {
+
+            try {
+                await signUp(userImport).then (el => {
+                    expect (el.body.status).toBe('success')
+                    console.log(el.body, 'res')
+                })
+                await logIn({
+                    email: '',
+                    password : userImport.password,
+                }).then(el2 =>  {
+                    expect (el2.body.status).toBe('fail')
+                    expect(el2.body.message).toBe('Please provide email and password!')
+                    console.log(el2.body, 'res')
+                })
+
+            } catch (error) {
+                console.log ('Error during login process', error);
+                // throw Error (error)
+            }
+        })
+
+        it.only('get error when trying login without email using .end without Promise - option 4', (done) => {
+            signUp2({
+                email: userImport.email,
+                password : '',
+            }).end ((err,res) => {
+                if(err) return done (err)
+                expect (res.body.status).toBe('error');
+                expect(res.body.message).toBe('User validation failed: name: Please tell us your name!, password: Please provide a password, passwordConfirm: Please confirm your password')
+                done()
             })
         })
 
+        // it('login user option 5 using .end without Promise', (done) => {
+        //     signUp2(userImport).end ((err,res) => {
+        //         if(err) return done (err)
+        //         expect (res.body.status).toBe('success');
+        //         done()
+        //     })
+        // })
+
+
         it('get error when trying login with wrong password', async () => {
-            // await signUp(userImport).then (el => {
-            //     expect (el.body.status).toBe('success')
-            //     console.log(el.body, 'res')
-            // })
+            await signUp(userImport).then (el => {
+                expect (el.body.status).toBe('success')
+                console.log(el.body, 'res')
+            })
             await logIn({
                 email: userImport.email,
                 password : '123123',
@@ -79,10 +185,10 @@ describe('USER LOGIN', () => {
         })
 
         it('get error when trying login with wrong user email', async () => {
-            // await signUp(userImport).then (el => {
-            //     expect (el.body.status).toBe('success')
-            //     console.log(el.body, 'res')
-            // })
+            await signUp(userImport).then (el => {
+                expect (el.body.status).toBe('success')
+                console.log(el.body, 'res')
+            })
             await logIn({
                 email: '13132@mail.com',
                 password : userImport.password,
@@ -93,5 +199,4 @@ describe('USER LOGIN', () => {
 
         })
     })
-})
 
